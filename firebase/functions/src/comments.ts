07c12c987ce5f7cf84974fd1e8d672ab26549a6e
AndresCUd <acgonzalezh@correo.udistrital.edu.co>
2019-12-04 09:30:17 -0500

    update_location

--- firebase/functions/src/comments.ts
+++ firebase/functions/src/comments.ts
@@ -14,29 +14,26 @@ export const new_comment = functions.https.onCall(async (data, context) => {
   let photoUrl: String;
   let geohash: String;
   let location: GeoPoint;
-  if (typeof data.comment === "string" || data.comment instanceof String) {
+  if (typeof data.comment === "string" &&  data.comment.length <= 140 ) {
     comment = data.comment;
   } else {
     comment = "";
   }
   if (
-    typeof data.currentStatus === "string" ||
-    data.currentStatus instanceof String
+    typeof data.currentStatus === "string" 
   ) {
     currentStatus = data.currentStatus;
   } else {
     currentStatus = "";
   }
-  if (typeof data.photoUrl === "string" || data.photoUrl instanceof String) {
+  if (typeof data.photoUrl === "string" ) {
     photoUrl = data.photoUrl;
   } else {
     photoUrl = "";
   }
   if (
-    typeof data.location[0] === "number" ||
-    (data.location[0] instanceof Number &&
-      typeof data.location[1] === "number") ||
-    data.location[1] instanceof Number
+    typeof data.location[0] === "number"  &&
+      typeof data.location[1] === "number"
   ) {
     if (
       data.location[0] >= -90 &&
@@ -68,20 +65,33 @@ export const new_comment = functions.https.onCall(async (data, context) => {
   };
   return await addToFIreBase(commentData, "Comments");
 });
-/*
-.exit
-npm run build
-firebase functions:shell
- data = {
-  "comment":20,
-  "location":[ -11.54,45.4 ],
-  "currentStatus":"ajsdh",
-  "photoUrl":"asd"
-}
-new_comment(data)
 
-
-*/
+export const update_location = functions.https.onCall(async (data, context) => {
+  const user_uid = context.auth ? context.auth.uid : ''
+  if (user_uid === '') {
+    return { res: '400', msg:'Usuario no existe'}
+  }
+  let location: GeoPoint;
+  let geohash: String;
+  if (
+    data.location[0] >= -90 &&
+    data.location[0] <= 90 &&
+    data.location[1] >= -90 &&
+    data.location[1] <= 90
+  ) {
+    location = new GeoPoint(data.location[0], data.location[1]);
+    geohash = Geohash.encode(data.location[0], data.location[1]);
+  } else {
+    return {
+      res: "No es una ubicacion Valida"
+    };
+  }
+  const updateData = {
+    location:location,
+    geohash:geohash,
+  }
+return await update(updateData,'users',user_uid);
+})
 
 function addToFIreBase(data: any, collection: any) {
   return new Promise((resolve, reject) => {
@@ -98,4 +108,20 @@ function addToFIreBase(data: any, collection: any) {
       });
   });
 }
-//943.05
+
+function update(data: any, collection: any,id:any) {
+  return new Promise((resolve, reject) => {
+      const userRef = db.collection(collection).doc(id)
+      userRef.update(data).then(function () {
+          resolve({
+              res: '200'
+          })
+      }).catch(function (error) {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+          resolve({
+              res: '4c014'
+          })
+      });
+  })
+}
